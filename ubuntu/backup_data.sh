4a41de69ed5dada40e6b149e5b8fecab1c64e15e
Tarun Gopalkrishna A <tarun.gopalkrishna@kampd.com>
2022-08-07 10:50:20 +0530

    update: fixed issue and added more config

--- ubuntu/backup_data.sh
+++ ubuntu/backup_data.sh
@@ -5,11 +5,13 @@
 # $2 is the destination
 
 # Export the data directory
-src_data_dir=$1/data
+src_data_dir=$1/data/
 echo $src_data_dir
 dst_data_dir=$2/data_dir
-rsync -avh --no-links --progress --exclude 'anaconda3' --exclude "node_modules" --exclude "nltk_data" $src_data_dir $dst_data_dir
+rsync -avh --no-links --progress --exclude-from='./ignore_data_files.txt' $src_data_dir $dst_data_dir
 
 # Export the config details
 dst_home_dir=$2/home_data_dir
-rsync -avh ---no-links --progress --exclude-from='./ignore_files.txt' $1 $dst_home_dir
+rsync -avh --no-links --progress --exclude-from='./ignore_home_files.txt' $1 $dst_home_dir
+
+# Export other configurations
